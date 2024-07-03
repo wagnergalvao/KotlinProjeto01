8742b024c05115228ced24e4afb6d5da1c33189a
WGalvao <wagnerjbgalvao@gmail.com>
2024-07-03 17:55:07 -0300

    Seção 03 Aula 13 Adicionando evento de clique em um botão

--- app/src/main/java/com/wgalvao/udemy/kotlin/kotlinprojeto01/MainActivity.kt
+++ app/src/main/java/com/wgalvao/udemy/kotlin/kotlinprojeto01/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.wgalvao.udemy.kotlin.kotlinprojeto01
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun clique(view: View) {
+        println("Ok, você clicou!")
     }
 }
